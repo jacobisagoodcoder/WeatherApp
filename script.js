b07216204cc156e8417c553cac78a21fbbdcfeae
Jacob Van Dyke <jacobvandyke194@gmail.com>
2024-03-11 18:50:24 -0600

    added more weather info based on location into dom from api call

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const elevationElement = document.getElementById('elevation');
 const mainElement = document.getElementById('weatherForecastMain');
+const elevationElement = document.getElementById('elevation');
+const tempMax = document.getElementById('tempMax');
+const tempMin = document.getElementById('tempMin');
+const rain = document.getElementById('rain');
 
 function getLocation(){
     try {
@@ -23,16 +26,21 @@ function error(){
 }
 
 async function getWeather(lat, long){
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}`;
-
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,rain,showers,snowfall&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch`
     try{
         const response = await fetch(url);
         const weather = await response.json();
-
+        console.log(weather)
         if(response){
-            const elevationFt = weather.elevation * 3.28;
+            const elevationFt = Math.floor(weather.elevation * 3.28);
             mainElement.classList.remove('hide');
+
             elevationElement.innerHTML = `You are currently ${elevationFt} ft above sea level!`
+            tempMax.innerHTML = `Today the current high is ${Math.floor(weather.daily.temperature_2m_max[0])}\u00B0F!`
+            tempMin.innerHTML = `Today the current low is ${Math.floor(weather.daily.temperature_2m_min[0])}\u00B0F!`
+
+            rain.innerHTML = `You are currently expected to have ${Math.floor(weather.hourly.rain[0])} inches of rain!`
+
         }
     } catch(err){
         console.error(err);
